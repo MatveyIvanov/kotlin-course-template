f5872d397c2d69ad35a60f6c9aa9941c9a88683a
Matvey <cool.matvey250301@gmail.com>
2021-10-23 23:02:40 +0300

    Left alignment version 1.1 + Unit tests

--- src/main/kotlin/textAlignment/Text.kt
+++ src/main/kotlin/textAlignment/Text.kt
@@ -8,14 +8,24 @@ enum class Alignment {
 }
 
 class Text(_text: String = "", _width: Int = 120, _alignment: Alignment = Alignment.LEFT) {
-    private var text: String = _text
-    private var width = _width
+    /*
+    *   I decided to keep the original text separate from the aligned one.
+    *   This is because I don’t know how to distinguish between user-supplied
+    *   line breaks and line breaks that appear when aligned.
+    *   After all, when re-aligning the text, I cannot remove user's line breaks, but I have to remove "my own".
+    *   Therefore, when aligning, the original text is taken.
+    */
+    private var sourceText: String = _text
+    private var alignedText = ""
+    var width = _width
     private var alignment: Alignment = _alignment
 
-    operator fun plus(toAdd: String): Text = Text(this.text + toAdd, this.width, this.alignment)
-    operator fun plus(toAdd: Text): Text = Text(this.text + toAdd.text, this.width, this.alignment)
+    operator fun plus(toAdd: String): Text = Text(sourceText + toAdd, width, alignment)
+    operator fun plus(toAdd: Text): Text = Text(sourceText + toAdd.sourceText, width, alignment)
 
-    override fun toString(): String = text
+    override fun toString(): String {
+        return alignedText.ifEmpty { sourceText }
+    }
 
     fun align(): String = when(alignment) {
         Alignment.LEFT -> alignLeft()
@@ -24,44 +34,83 @@ class Text(_text: String = "", _width: Int = 120, _alignment: Alignment = Alignm
         Alignment.JUSTIFY -> ""
     }
 
+    // Left alignment
     private fun alignLeft(): String {
-        var curPosition = 0
-        var curWordLength = 0
-        var curWord = ""
-        var newText: String = "_".repeat(width) + "\n"
-        for (letter in text) {
+        /*
+        *   Algorithm loop through the text, takes word by word and process them according to the next rules:
+        *     - If word fits in the current line, then add it to the current line
+        *     - If word does not fit in the current line, then add word to the new line
+        *     - If word's length is bigger than width, then fill the rest of the current line with
+        *           part of the word and split the rest of the word along the following lines
+        */
+        var curPosition = 0 // Current position of the current line
+        var curWord = "" // Current word
+        // var newText: String = "_".repeat(width) + "\n" // Aligned text
+        var newText = ""
+        for (letter in sourceText) {
             when (letter) {
                 '\n' -> {
-                    newText += if (width - curPosition >= curWordLength) "$curWord\n" else "\n$curWord\n"
-                    curPosition = 0
-                    curWordLength = 0
-                    curWord = ""
+                    newText += if (width - curPosition >= curWord.length) "$curWord\n" else "\n$curWord\n"
+                    curPosition = 0 // New line
+                    curWord = "" // Reset current word
                 }
                 ' ' -> {
-                    if (width - curPosition >= curWordLength) {
-                        newText += curWord
-                        curPosition += curWordLength + 1
-                        if (width >= curPosition) {
+                    if (curWord.length > width) { // If word's length is bigger than line width then split the word
+                        if (width < curPosition) { // Go to next line if current line is fully filled
+                            newText += "\n"
+                            curPosition = 0
+                        }
+                        newText += curWord.substring(0, width - curPosition) + "\n" // Fill the rest of the current line with the part of the word
+                        curWord = curWord.drop(width - curPosition) // Delete that part
+                        while (curWord.length >= width) { // While the rest of the word is bigger than line width
+                            newText += curWord.substring(0, width) + "\n" // Fill the line with the part of the word and go to next line
+                            curWord = curWord.drop(width) // Delete that part
+                        }
+                        newText += curWord // Add the last part of the word to current line
+                        curPosition += curWord.length // Change current position
+                        curWord = "" // Reset current word
+                    }
+                    if (width - curPosition >= curWord.length) { // If there is enough place to put the word
+                        newText += curWord // Add word to the current line
+                        curPosition += curWord.length + 1 // Change current position (including space)
+                        if (width >= curPosition) { // If it's not EOL, add space after last word
                             newText += " "
                         }
-                    } else {
-                        if (width - curPosition > 0)
-                            newText += " ".repeat(width - curPosition)
-                        newText += "\n$curWord "
-                        curPosition = curWordLength + 1
+                    } else { // If there is not enough place to put the word
+                        newText += if (curWord.length == width) "\n$curWord" else "\n$curWord " // Add new word
+                        curPosition = curWord.length + 1 // Change position
                     }
-                    curWordLength = 0
-                    curWord = ""
+                    curWord = "" // Reset current word
                 }
-                else -> {
-                    curWordLength++
+                else -> { // Get another letter of the word
                     curWord += letter
                 }
             }
         }
-        newText += if (width - curPosition >= curWordLength) "$curWord " else " ".repeat(width - curPosition) + "\n$curWord "
 
-        text = newText
-        return text
+        // Process the last word (same as other words)
+        if (curWord.length > width) { // If word's length is bigger than line width then split the word
+            if (width < curPosition) {
+                newText += "\n"
+                curPosition = 0
+            }
+            newText += curWord.substring(0, width - curPosition) + "\n"
+            curWord = curWord.drop(width - curPosition)
+            while (curWord.length >= width) {
+                newText += curWord.substring(0, width) + "\n"
+                curWord = curWord.drop(width)
+            }
+            newText += curWord
+        }
+        else if (width - curPosition >= curWord.length) { // If there is enough place to put the word
+            newText += curWord
+            curPosition += curWord.length + 1
+        }
+        else { // If there is not enough place to put the word
+            newText += "\n$curWord"
+        }
+
+        alignedText = newText // Replace old text with aligned one
+        return alignedText
     }
 }
---
+++ src/test/kotlin/textAlignmentTests/TextTest.kt
@@ -0,0 +1,89 @@
+package textAlignmentTests
+
+import textAlignment.Text
+import org.junit.Assert
+import org.junit.Test
+
+
+const val TEXT1 = "Sometimes you still make small grammar mistakes, and I think you can improve your vocabulary. I recommend you review many of the language points we studied this term."
+const val TEXT2 = "Параллелепипед - это геометрическая фигура"
+
+class TextTest {
+
+    @Test
+    fun test_alignment_with_default_width() {
+        val text = Text(TEXT1)
+        Assert.assertEquals(
+            "Sometimes you still make small grammar mistakes, and I think you can improve your vocabulary. I recommend you review \n" +
+                    "many of the language points we studied this term.", text.align()
+        )
+    }
+
+    @Test
+    fun test_alignment_with_custom_width() {
+        val text = Text(TEXT1, 55)
+        Assert.assertEquals("Sometimes you still make small grammar mistakes, and I \n" +
+                "think you can improve your vocabulary. I recommend you \n" +
+                "review many of the language points we studied this \n" +
+                "term.", text.align())
+    }
+
+    @Test
+    fun test_alignment_keeping_punctuation_mark_with_word() {
+        // This test checks if punctuation mark is tied to the word when word right at the end of the line
+        val text = Text(TEXT1, 47)
+        /* There is no enough space for 'mistakes' just because of comma,
+           but it still moves to the next line with this comma
+         */
+        Assert.assertEquals("Sometimes you still make small grammar \n" +
+                "mistakes, and I think you can improve your \n" +
+                "vocabulary. I recommend you review many of the \n" +
+                "language points we studied this term.", text.align())
+        text.width = 48
+        /* There is enough space for 'mistakes' + comma,
+           so it stays at "current" line
+         */
+        Assert.assertEquals("Sometimes you still make small grammar mistakes,\n" +
+                "and I think you can improve your vocabulary. I \n" +
+                "recommend you review many of the language points\n" +
+                "we studied this term.", text.align())
+    }
+
+    @Test
+    fun test_alignment_with_custom_width_last_word_perfectly_fits() {
+        val modifiedText = TEXT1.dropLast(1) // Remove last dot so 'term' will not be moved on a new line
+        val text = Text(modifiedText, 55)
+
+        Assert.assertEquals("Sometimes you still make small grammar mistakes, and I \n" +
+                "think you can improve your vocabulary. I recommend you \n" +
+                "review many of the language points we studied this term", text.align())
+    }
+
+    @Test
+    fun test_alignment_with_word_splitting_two_lines() {
+        val text = Text(TEXT2, 10)
+        Assert.assertEquals(
+            "Параллелеп\n" +
+                    "ипед - это\n" +
+                    "геометриче\n" +
+                    "ская \n" +
+                    "фигура", text.align()
+        )
+    }
+
+    @Test
+    fun test_alignment_with_word_splitting_multiple_lines() {
+        val text = Text(TEXT2, 5)
+        Assert.assertEquals(
+            "Парал\n" +
+                    "лелеп\n" +
+                    "ипед \n" +
+                    "- это\n" +
+                    "геоме\n" +
+                    "триче\n" +
+                    "ская \n" +
+                    "фигур\n" +
+                    "а", text.align()
+        )
+    }
+}
